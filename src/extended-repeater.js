5e1c4d485cfb51ac222927a669ce5e804e737b47
Olga Latkina <olga.latkina@gmail.com>
2020-02-23 11:12:55 +0300

    feat: add solution: extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,50 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    if (options === undefined) {
+        throw 'options: undefined';
+    }
+
+    let separator = options.separator;
+    if (separator === undefined) {
+        separator = '+';
+    }
+
+    let repeatTimes = options.repeatTimes;
+    if (repeatTimes === undefined) {
+        repeatTimes = 1; // NB: условия задачи не оговаривают defult для repeatTimes
+    }
+
+    let addition = options.addition;
+    if (addition === undefined) {
+        addition = ''; // NB: условия задачи не оговаривают defult для addition
+    }
+
+    let additionSeparator = options.additionSeparator;
+    if (additionSeparator === undefined) {
+        additionSeparator = '|';
+    }
+
+    let additionRepeatTimes = options.additionRepeatTimes;
+    if (additionRepeatTimes === undefined) {
+        additionRepeatTimes = 1; // NB: условия задачи не оговаривают defult для additionRepeatTimes
+    }
+
+    let addon = '' + str; // NB: convert to string
+    for (let j = 0; j < additionRepeatTimes; j++)
+    {
+        if (j > 0) {
+            addon += additionSeparator;
+        }
+        addon += addition;
+    }
+
+    let result = '';
+    for (let i = 0; i < repeatTimes; i++)
+    {
+        if (i > 0) {
+            result += separator;
+        }
+        result += addon;
+    }
+
+    return result;
 };
-  
