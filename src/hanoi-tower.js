65bad9d8fc52539e1ee133fd4d23b7ac87e18ee0
Olga Latkina <olga.latkina@gmail.com>
2020-02-23 12:39:18 +0300

    feat: add solution: hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,14 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed)
+{
+    let turns = 2 ** disksNumber - 1;
+
+    // NB: test fails due to rounding error
+    //     if calculated like:
+    //            turns /  turnsSpeed * 3600
+    let seconds = turns / (turnsSpeed / 3600);
+
+    return {
+        turns: turns,
+        seconds: seconds
+    };
 }
