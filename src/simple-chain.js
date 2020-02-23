747942137f620ee003928c2c07266f6185d25095
Olga Latkina <olga.latkina@gmail.com>
2020-02-23 18:54:10 +0300

    feat: add solution: simle-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,24 +1,42 @@
 const chainMaker = {
-  getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  }
+    separator: '~~',
+    chain: [],
+
+    getLength() {
+        return this.chain.length;
+    },
+
+    addLink(value) {
+        if (value !== undefined) {
+            this.chain = this.chain.concat([`( ${value} )`]);
+        } else {
+            this.chain = this.chain.concat(['( )']);
+        }
+        return this;
+    },
+
+    removeLink(position) {
+        if ((typeof position !== 'number') ||
+            (position !== Math.floor(position)) ||
+            (position < 1) || (position > this.chain.length))
+        {
+            this.chain = []; // NB: cleaning chain is required here, although this requirement is not specified
+            throw new Error('invalid position: ' + position);
+        }
+        this.chain.splice(position - 1, 1);
+        return this;
+    },
+
+    reverseChain() {
+        this.chain.reverse();
+        return this;
+    },
+
+    finishChain() {
+        let result = this.chain.join(this.separator);
+        this.chain = [];
+        return result;
+    }
 };
 
 module.exports = chainMaker;
