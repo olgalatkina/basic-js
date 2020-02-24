88bb2d87b062829582aa2d7dbc4e9e79eebf7aab
Olga Latkina <olga.latkina@gmail.com>
2020-02-24 15:07:39 +0300

    feat: add solution: transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,46 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr)
+{
+    if (!(arr instanceof Array)) {
+        throw new Error('wrong parameter type: ' + (typeof arr));
+    }
+
+    let transformed = [];
+    for (let i = 0; i < arr.length; i++)
+    {
+        let elem = arr[i];
+        if (elem === '--discard-next')
+        {
+            i++;
+            continue;
+        }
+
+        if (elem === '--discard-prev')
+        {
+            let l = transformed.length;
+            if (l > 0) {
+                transformed.pop();
+            }
+            continue;
+        }
+
+        if (elem === '--double-next')
+        {
+            if (i + 1 < arr.length) {
+                transformed.push(arr[i + 1]);
+            }
+            continue;
+        }
+
+        if (elem === '--double-prev')
+        {
+            if (i - 1 >= 0) {
+                transformed.push(arr[i - 1]);
+            }
+            continue;
+        }
+
+        transformed.push(elem);
+    }
+
+    return transformed;
 };
