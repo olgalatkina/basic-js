9c8623c108871033ab402b1c67103f61f73ed2bd
Olga Latkina <olga.latkina@gmail.com>
2020-03-07 18:00:21 +0300

    feat: add solution: what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,20 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+    const seasons = [
+        'winter', 'winter',
+        'spring', 'spring', 'spring',
+        'summer', 'summer', 'summer',
+        'autumn', 'autumn', 'autumn',
+        'winter']
+
+    if (!date) {
+        return 'Unable to determine the time of year!';
+    }
+
+    if (isNaN(Date.parse(date)) || !date.getTime()) {
+        throw new Error('Unable to determine the time of year!');
+    }
+
+    let month = new Date(date).getMonth();
+
+    return seasons[month];
 };
